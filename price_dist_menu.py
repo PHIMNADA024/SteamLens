5fc3dcf1062366ba4c3d989dd594fda0dd7d5e12
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-04-29 23:31:57 +0700

    Create PriceDistMenu class

---
+++ price_dist_menu.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+from graph_controller import GraphController
+
+
+class PriceDistMenu(tk.Frame):
+    """
+    A class representing a frame for displaying the price distribution graph and descriptive statistics.
+    """
+
+    def __init__(self, parent, font, **kwargs) -> None:
+        """
+        Initializes the SearchMenu frame.
+
+        :param parent: The parent widget.
+        :param font: The font to be used in the PriceDistMenu.
+        """
+        super().__init__(parent, **kwargs)
+        self.config(background="#2A475E")
+        self.parent = parent
+        self.font = font
+        self.graph_controller = GraphController()
+        self.init_components()
+
+    def init_components(self) -> None:
+        """
+        Initializes the UI components of the price distribution menu.
+        """
+        self.init_graph_frame()
+        self.init_stat_frame()
+
+    def init_graph_frame(self) -> None:
+        """
+        Initializes the frame for displaying the price distribution graph.
+        """
+        padding = {'padx': 15, 'pady': 10}
+        option = {'font': (self.font, 26)}
+
+        self.graph_frame = tk.Frame(self, background="#2A475E")
+        self.graph_frame.pack(side=tk.LEFT, **padding)
+
+        price_label = tk.Label(self.graph_frame, text="Price of Game in Steam", foreground="white",
+                               background="#2A475E", **option)
+        price_label.pack(**padding)
+
+        graph_widget = self.graph_controller.price_dist_graph(self.graph_frame)
+        graph_widget.pack(fill=tk.BOTH, expand=True)
+
+    def init_stat_frame(self) -> None:
+        """
+        Initializes the frame for displaying descriptive statistics.
+        """
+        padding = {'padx': 15, 'pady': 10}
+        option = {'font': (self.font, 26)}
+
+        self.stat_frame = tk.Frame(self, background="#2A475E")
+        self.stat_frame.pack(side=tk.RIGHT, padx=30)
+
+        stat_label = tk.Label(self.stat_frame, text="Descriptive Statistics", foreground="white",
+                               background="#2A475E", **option)
+        stat_label.pack(**padding)
+
+        descriptive_stats_label = tk.Label(self.stat_frame, text=self.get_descriptive_stats_text(), foreground="white",
+                                           background="#2A475E", font=(self.font, 22), justify=tk.LEFT)
+        descriptive_stats_label.pack(**padding)
+
+    def get_descriptive_stats_text(self) -> str:
+        """
+        Retrieves the descriptive statistics text.
+
+        :returns: The descriptive statistics text.
+        """
+        descriptive_stats = (
+            f"Count: {self.graph_controller.price_dist_statistics()['count']}\n"
+            f"Mean: {self.graph_controller.price_dist_statistics()['mean']:.2f}\n"
+            f"Std Deviation: {self.graph_controller.price_dist_statistics()['std']:.2f}\n"
+            f"Minimum: {self.graph_controller.price_dist_statistics()['min']:.2f}\n"
+            f"25th Percentile: {self.graph_controller.price_dist_statistics()['25%']:.2f}\n"
+            f"Median: {self.graph_controller.price_dist_statistics()['50%']:.2f}\n"
+            f"75th Percentile: {self.graph_controller.price_dist_statistics()['75%']:.2f}\n"
+            f"Maximum: {self.graph_controller.price_dist_statistics()['max']:.2f}\n"
+        )
+        return descriptive_stats
