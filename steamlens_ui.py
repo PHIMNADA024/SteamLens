582e5ad2e66e9a3c0270c4a685678199af02110d
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-05-04 00:20:23 +0700

    Update SteamLensUI class

--- steamlens_ui.py
+++ steamlens_ui.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from home_menu import HomeMenu
 from search_menu import SearchMenu
+from price_dist_menu import PriceDistMenu
+from released_year_menu import ReleasedYearMenu
+from relationship_menu import RelationshipMenu
+from dashboard_menu import DashboardMenu
 
 
 class SteamLensUI(tk.Tk):
@@ -17,6 +21,17 @@ class SteamLensUI(tk.Tk):
         self.config(background="#2A475E")
         self.geometry("1280x720")
         self.font = "Segoe UI"
+
+        self.home_menu = HomeMenu(self, self.font)
+        self.search_menu = SearchMenu(self, self.font)
+        self.price_dist_menu = PriceDistMenu(self, self.font)
+        self.released_year_menu = ReleasedYearMenu(self, self.font)
+        self.relationship_menu = RelationshipMenu(self, self.font)
+        self.dashboard_menu = DashboardMenu(self, self.font)
+
+        self.menus = [self.home_menu, self.search_menu, self.price_dist_menu, self.released_year_menu,
+                      self.relationship_menu, self.dashboard_menu]
+
         self.init_components()
 
     def init_components(self) -> None:
@@ -29,6 +44,19 @@ class SteamLensUI(tk.Tk):
         menubar = tk.Menubutton(menu_frame, text="Menu", font=(self.font, 18))
         menubar.pack(side=tk.LEFT)
         menu_choice = tk.Menu(menubar)
+        menu_choice.add_command(label="Home",
+                                command=lambda: self.change_to_menu(self.home_menu))
+        menu_choice.add_separator()
+        menu_choice.add_command(label="Search",
+                                command=lambda: self.change_to_menu(self.search_menu))
+        menu_choice.add_command(label="Prices Distribution",
+                                command=lambda: self.change_to_menu(self.price_dist_menu))
+        menu_choice.add_command(label="Release Year",
+                                command=lambda: self.change_to_menu(self.released_year_menu))
+        menu_choice.add_command(label="Relationship",
+                                command=lambda: self.change_to_menu(self.relationship_menu))
+        menu_choice.add_command(label="Games Insights Dashboard",
+                                command=lambda: self.change_to_menu(self.dashboard_menu))
         menubar.config(menu=menu_choice)
         menu_frame.pack(fill=tk.X)
 
@@ -38,17 +66,17 @@ class SteamLensUI(tk.Tk):
                          background="#1B2838", foreground="white")
         title.pack(fill=tk.X)
 
-        self.home_menu = HomeMenu(self, self.font)
-        self.home_menu.pack(**padding)
+        # TODO: Change this to Home menu before push
+        self.dashboard_menu.pack(**padding)
 
-        self.search_menu = SearchMenu(self, self.font)
+    def change_to_menu(self, new_menu):
+        padding = {'padx': 20, 'pady': 20}
 
-    def switch_to_search_menu(self) -> None:
-        """
-        Switches to the search menu.
-        """
-        self.winfo_children()[-2].pack_forget()
-        self.search_menu.pack()
+        for menu in self.menus:
+            if menu != new_menu:
+                menu.pack_forget()
+
+        new_menu.pack(**padding)
 
     def run(self) -> None:
         """
